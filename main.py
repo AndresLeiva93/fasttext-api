ea0d5b135e2f0e9e3ea4dbb6d8b999abb61802fe
AndresLeiva93 <89427059+AndresLeiva93@users.noreply.github.com>
2025-04-23 10:26:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -49,3 +49,11 @@ async def comparar(request: Request):
         "marca_parecida": marcas[idx_max],
         "similitud": round(similitudes[idx_max] * 100, 2)
     }
+
+if __name__ == "__main__":
+    import uvicorn
+    import os
+
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
+
